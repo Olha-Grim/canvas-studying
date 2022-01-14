dd55412248d1839710314691d359c0f496ac7bfa
Olha-Grim <olha.hrymchak@prea.ws>
2022-01-14 15:56:47 +0200

    painting in canvas

--- src/modules/canvas/index.js
+++ src/modules/canvas/index.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { CanvasWrapper } from "./styled";
 
 export const Canvas = () => {
+  const [myColor, setColor] = useState("red");
   const canvasRef = useRef(null);
   const canvasRef2 = useRef(null);
+  const canvasRef3 = useRef(null);
+  const inputRef = useRef(null);
 
   useEffect(() => {
     const canvasSquares = canvasRef.current;
@@ -13,7 +16,11 @@ export const Canvas = () => {
     const canvasLines = canvasRef2.current;
     const ctx2 = canvasLines.getContext("2d");
     createCanvasLines(ctx2);
-  }, []);
+
+    const canvasPaints = canvasRef3.current;
+    const ctx3 = canvasPaints.getContext("2d");
+    createCanvasPainting(ctx3, canvasPaints);
+  }, [myColor]);
 
   function createCanvasSquares(ctx1) {
     ctx1.fillStyle = "red"; //fill with color
@@ -75,10 +82,34 @@ export const Canvas = () => {
     ctx2.fillStyle = "pink";
     ctx2.fill();
   }
+
+  function createCanvasPainting(ctx3, canvasPaints) {
+    canvasPaints.onmousedown = function () {
+      canvasPaints.onmousemove = function (event) {
+        let x = event.offsetX;
+        let y = event.offsetY;
+        //   ctx3.fillRect(x-5, y-5, 10, 10); //auto-paints
+        ctx3.lineWidth = "2";
+        ctx3.fillStyle = myColor;
+        ctx3.lineCap = "round";
+        ctx3.fillRect(x - 5, y - 5, 10, 10);
+      };
+
+      canvasPaints.onmouseup = function () {
+        canvasPaints.onmousemove = null;
+      };
+    };
+// console.log(inputRef.current.oninput)
+    inputRef.current.oninput = function (event) {
+      setColor(event.target.value);
+    };
+  }
   return (
     <CanvasWrapper>
       <canvas id="c1" width="400" height="200" ref={canvasRef}></canvas>
       <canvas id="c1" width="400" height="200" ref={canvasRef2}></canvas>
+      <canvas id="c1" width="400" height="200" ref={canvasRef3}></canvas>
+      <input type="color" id="color" ref={inputRef} />
     </CanvasWrapper>
   );
 };
